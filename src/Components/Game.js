894cd117da8c88a1521793ba0f394d0915b5af91
Pipetboy <felipefelizx@gmail.com>
2023-03-09 19:15:36 -0300

    Feature: Ball en movimiento

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../Style/Game.css'
 import Paddle from './Paddle';
 import Brick from './Brick';
@@ -6,6 +6,10 @@ import Ball from './Ball';
 
 function Game() {
 
+    const [ballPosition, setBallPosition] = useState({ x: 100, y: 200 });
+    const [ballVelocity, setBallVelocity] = useState({ x: 5, y: 5 });
+
+    //... código para crear ladrillos
     const bricks = [];
     for (let i = 0; i < 24; i++) {
         const colorIndex = i % 7;
@@ -21,11 +25,40 @@ function Game() {
             hitCount
         });
     }
+
+    useEffect(() => {
+        // Esta función se ejecuta en cada fotograma
+        const updateBallPosition = () => {
+            // Verificar colisiones con los bordes del contenedor
+            if (ballPosition.x + ballVelocity.x > 570 - 10 || ballPosition.x + ballVelocity.x < 10) {
+                setBallVelocity(prevVelocity => ({ x: -prevVelocity.x, y: prevVelocity.y }));
+            }
+            if (ballPosition.y + ballVelocity.y < 10) {
+                setBallVelocity(prevVelocity => ({ x: prevVelocity.x, y: -prevVelocity.y }));
+            }
+            if (ballPosition.y + ballVelocity.y > 300 - 10) {
+                // La pelota ha llegado al borde inferior, reiniciar el juego
+                setBallPosition({ x: 100, y: 200 });
+                setBallVelocity({ x: 5, y: 5 });
+                return;
+            }
+            // Actualiza la posición de la bola
+            setBallPosition(prevPosition => ({
+                x: prevPosition.x + ballVelocity.x,
+                y: prevPosition.y + ballVelocity.y
+            }));
+        };
+
+        const animationId = setInterval(updateBallPosition, 16); // Actualiza la posición de la bola cada 16ms
+
+        return () => clearInterval(animationId); // Limpia el intervalo cuando el componente se desmonta
+    }, [ballVelocity, ballPosition]);
+
+
     return (
         <div className="container">
             <div className="contenedor">
-                <Ball position={{ x: 100, y: 200 }} radius={10} color={'#000'} />
-
+                <Ball position={ballPosition} radius={10} color={'#000'} />
                 <Paddle position={100} width={100} height={10} color={'#000'} />
                 {bricks.map((brick, index) => (
                     <Brick key={index} {...brick} />
